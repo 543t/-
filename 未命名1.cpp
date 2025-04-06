e1d7d1067c5cef6f4f3a91897a5410d21a43ca33
543t <148614036+543t@users.noreply.github.com>
2025-04-06 22:30:55 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+string& fun(string& s1,int nums)
+{
+	
+}
+int main()
+{
+	int a  = 10;
+	const int& ai = a;
+	cout<<ai<<endl;
+	cout<<++a<<endl;
+	cout<<ai<<endl;
+}
